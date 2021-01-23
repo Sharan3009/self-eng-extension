13213c62b3a1c48585817dbd0b37da9beb6fd05a
Sharan3009 <sharansingh3009@gmail.com>
2021-01-23 20:57:17 +0530

    added bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import $ from "jquery";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
