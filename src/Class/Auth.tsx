83aed9b149d405605ddfa15c0b892829c5372644
Sharan3009 <sharansingh3009@gmail.com>
2021-01-24 00:43:27 +0530

    changed return error to throw error

--- src/Class/Auth.tsx
+++ src/Class/Auth.tsx
@@ -3,14 +3,13 @@ import storage from "./Storage";
 class Auth {
 
     private authKey:string = "authToken";
-    
+
     public isAuthenticated = async ():Promise<any> => {
         const token:string = await storage.get<string>(this.authKey);
-        console.log(token)
         if(token){
-            return token
+            return token;
         }
-        return new Error();
+        throw new Error();
     }
 }
 
