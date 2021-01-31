1c9e3f44fec9ba227d7bdc5deeac850fc018317c
Sharan3009 <sharansingh3009@gmail.com>
2021-01-31 17:29:54 +0530

    setting form value

--- src/components/auth/signup.tsx
+++ src/components/auth/signup.tsx
@@ -1,10 +1,11 @@
-import React, { Component, FormEvent } from "react";
+import { Component, FormEvent } from "react";
 import {RouteComponentProps } from "react-router-dom";
 import SignupButton from "../shared/authButton/authButton";
 import { TextField } from "@material-ui/core";
 import { compose, Store } from "redux";
 import { connect } from "react-redux";
 import { ISignUp } from "../../Interface/CredentialForm";
+import {setFormData} from "../../actions/auth/signup";
 
 class SignUp extends Component<RouteComponentProps&ISignUp&Store> {
 
@@ -13,41 +14,54 @@ class SignUp extends Component<RouteComponentProps&ISignUp&Store> {
         alert(1)
     }
 
+    private setForm = (e:FormEvent<HTMLInputElement|HTMLTextAreaElement>) => {
+        const {name,value} = (e.target as HTMLInputElement);
+        this.props.dispatch(setFormData(name,value))
+      }
+
     render(){
 
         const {name,email,password,confirmPassword} = this.props;
         return <form className="absolute-center" onSubmit={this.signup}>
                     <TextField
                         label="Name"
+                        name="name"
                         variant="outlined"
                         type="text"
                         className="pb-3 wpx-240"
                         size="small"
                         value={name}
+                        onChange={this.setForm}
                     />
                     <TextField
                         label="Email"
+                        name="email"
                         variant="outlined"
                         type="email"
                         className="pb-3 wpx-240"
                         size="small"
                         value={email}
+                        onChange={this.setForm}
                         />
                     <TextField
                         label="Password"
+                        name="password"
                         variant="outlined"
                         type="password"
                         className="pb-3 wpx-240"
                         size="small"
                         value={password}
+                        onChange={this.setForm}
                         />
                     <TextField
                         label="Confirm Password"
+                        name="confirmPassword"
                         variant="outlined"
                         type="password"
                         className="pb-3 wpx-240"
                         size="small"
                         value={confirmPassword}
+                        onChange={this.setForm}
                         />
                     <SignupButton text="Sign up"/>
                 </form>
