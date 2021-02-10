6bfcfa069d909a5b175d47cb692a4e44ebd9a037
Sharan3009 <sharansingh3009@gmail.com>
2021-02-10 14:37:07 +0530

    added loader instead of Loading...

--- src/HOC/AuthenticatedRedirect.tsx
+++ src/HOC/AuthenticatedRedirect.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Route,Redirect } from 'react-router-dom';
 import { RouteProps } from "react-router";
 import Auth from '../Class/Auth';
+import Loader from "../components/shared/loader/loader";
 
 const AuthenticatedRedirectRoute = ({ component: Component, ...rest }:RouteProps):JSX.Element => {
   const [state, setState]:[string,React.Dispatch<React.SetStateAction<string>>] = useState("loading");
@@ -14,7 +15,7 @@ const AuthenticatedRedirectRoute = ({ component: Component, ...rest }:RouteProps
     })
   });
 
-  if(!Component || state==="loading") return <div>Loading...</div>;
+  if(!Component || state==="loading") return <Loader isLoader={true}/>;
 
   return <Route {...rest}
     render={(props) =>
