aafb4c23cb44835d8ab3022b8157fd3770b2508f
Sharan3009 <sharansingh3009@gmail.com>
2021-02-10 18:44:43 +0530

    socket connect

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,32 @@ import React from 'react';
 import './App.scss';
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Entry from "./components/auth/entry";
-import Login from "./components/auth/login";
 import SignUp from "./components/auth/signup";
 import AuthenticatedRedirect from "./HOC/AuthenticatedRedirect";
 import Home from './components/main/home';
-import { Provider } from "react-redux";
-import store from "./store";
+import { connect } from "react-redux";
+import { socketConnect } from './actions/socket';
 
-class App extends React.Component {
-    
-    render(){
-      return(
-        <Provider store={store}>
-          <BrowserRouter>
-            <Switch>
-              <AuthenticatedRedirect exact path="/entry" component={Entry} />
-              <AuthenticatedRedirect exact path="/signup" component={SignUp}/>
-              <Route exact path="/home" component={Home} />
-              <Route path="/">
-                <Redirect to="/entry"/>
-              </Route>
-            </Switch>
-          </BrowserRouter>
-        </Provider>
-      )
-    }
+class App extends React.Component<any> {
+
+  componentDidMount(){
+    this.props.dispatch(socketConnect());
+  }
+  
+  render(){
+    return(
+      <BrowserRouter>
+        <Switch>
+          <AuthenticatedRedirect exact path="/entry" component={Entry} />
+          <AuthenticatedRedirect exact path="/signup" component={SignUp}/>
+          <Route exact path="/home" component={Home} />
+          <Route path="/">
+            <Redirect to="/entry"/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    )
+  }
 }
 
-export default App;
+export default connect()(App);
--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,14 @@ import App from './App';
 import $ from "jquery";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import reportWebVitals from './reportWebVitals';
+import store from "./store";
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
