3055333462fe1fb62f4636195a563f156fdc0684
Sharan3009 <sharansingh3009@gmail.com>
2021-02-19 23:05:37 +0530

    working with ts

--- background.ts
+++ background.ts
@@ -1,17 +1,19 @@
-chrome.runtime.onMessage.addListener(function (requestObj, sender, sendMessage) {
+import { Action } from "./src/Interface/Action";
+import {host, apiVersion} from "./src/config";
+chrome.runtime.onMessage.addListener((requestObj:Action, sender:chrome.runtime.MessageSender, sendMessage) => {
 
-    let payload = requestObj.payload;
+    let payload:any = requestObj.payload;
 
     switch (requestObj.type) {        
 
         case "GOOGLE_LOGIN": {
-            let popupWidth = 460;
-            let popupHeight=560;
-            chrome.windows.create({ url: "http://localhost:3000/api/v1/google/auth", type: "popup",
+            let popupWidth:number = 460;
+            let popupHeight:number=560;
+            chrome.windows.create({ url: `${host}${apiVersion}google/auth`, type: "popup",
              width: popupWidth, height:popupHeight, 
              top:screen.height/2-popupHeight/2, 
              left:screen.width/2-popupWidth/2 }, (window) => {
-                chrome.tabs.onUpdated.addListener(function listener(tabId, changeInfo, tab) {
+                chrome.tabs.onUpdated.addListener(function listener(tabId:number, changeInfo, tab:chrome.tabs.Tab) {
                     if (window?.tabs && tabId === window?.tabs[0].id) {
                         if (tab.status === "complete" && tab.title) {
                             let [code, status, token] = tab.title.split(" - ");
@@ -34,6 +36,7 @@ chrome.runtime.onMessage.addListener(function (requestObj, sender, sendMessage) 
             })
             return false;
         }
-
     }
 });
+
+export {}
--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,31 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        background: path.resolve(__dirname,"background.js"),
-        contentScript: path.resolve(__dirname,"contentScript.js")
+        background: path.resolve(__dirname,"background.ts"),
+        contentScript: path.resolve(__dirname,"contentScript.ts")
     },
     mode: "development",
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'build'),
     },
+    resolve: {
+        extensions: ['.ts', '.js', '.json']
+    },
     // the 'transform-runtime' plugin tells Babel to
     // require the runtime instead of inlining it.
     module: {
         rules: [{
             loader: 'babel-loader',
-            test: /\.m?js$/,
+            test: /\.(js|jsx|tsx|ts)$/,
             exclude: /(node_modules|bower_components)/,
             options: {
                 presets: ['@babel/preset-env'],
             }
+        },{
+            loader: "ts-loader",
+            test: /\.(js|jsx|tsx|ts)$/,
+            exclude: /(node_modules|bower_components)/,
         }]
     }
 };
