39d683353470ea79ec4c632fbfeb2a234df8c06b
Sharan3009 <sharansingh3009@gmail.com>
2021-02-21 15:32:30 +0530

    dismiss popup on body click

--- contentScript.ts
+++ contentScript.ts
@@ -11,6 +11,9 @@ class ContentScript {
     }
 
     private addBodyDbClickEvent = ():void => {
+        $("body").on("click",(e:JQuery.ClickEvent)=>{
+            ($(".example-popover") as any).popover("hide");
+        })
         $("body").on("dblclick",(e:JQuery.DoubleClickEvent)=>{
             const selection:string|undefined = window.getSelection()?.toString()?.trim();
             if(selection){
@@ -27,7 +30,7 @@ class ContentScript {
     private addDivLayer = (e:JQuery.DoubleClickEvent):void => {
         let offset = $(e.currentTarget).offset();
         console.log(e.pageY,offset.top)
-        $("body").append(`<div style="position:absolute;left:${e.pageX-offset.left}px;top:${e.pageY-offset.top}px;" class="example-popover" data-toggle="popover" title="Selected word" data-content="${this.selection}"></div>`);
+        $("body").append(`<div style="position:absolute;left:${e.pageX-offset.left}px;top:${e.pageY-offset.top}px;" data-placement="top" class="example-popover" data-content="${this.selection}"></div>`);
         ($('.example-popover') as any).popover("show")
     }
 }
